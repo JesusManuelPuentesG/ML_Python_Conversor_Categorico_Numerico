ac07b25653ef8572a45725f6c10b1648d6c853bd
Jesús Manuel Puentes Gutiérrez <99046961+JesusManuelPuentesG@users.noreply.github.com>
2022-02-04 22:22:06 +0100

    Add files via upload

---
+++ Conversor_Categorico_Numerico.py
@@ -0,0 +1,11 @@
+…
+
+# Librería necesaria para convertir una característica
+# categórica en otra/s característica/s numéricas
+import pandas as pd
+
+…
+
+SERVING = pd.get_dummies(readDig.SERVING, prefix='SERVING')
+
+…
